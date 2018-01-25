ba61a7c8080f8cec3dd19b3e13acac9a58e42885
atheco <vdthanh@gmail.com>
2018-01-25 08:51:19 +0700

    new cong

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -28,24 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnCong = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // btnCong
             // 
-            this.button1.Location = new System.Drawing.Point(23, 51);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnCong.Location = new System.Drawing.Point(23, 51);
+            this.btnCong.Name = "btnCong";
+            this.btnCong.Size = new System.Drawing.Size(75, 23);
+            this.btnCong.TabIndex = 0;
+            this.btnCong.Text = "button1";
+            this.btnCong.UseVisualStyleBackColor = true;
+            this.btnCong.Click += new System.EventHandler(this.btnCong_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnCong);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -54,7 +55,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnCong;
     }
 }
 
--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -15,5 +15,13 @@ namespace Calculator
         {
             InitializeComponent();
         }
+
+        private void btnCong_Click(object sender, EventArgs e)
+        {
+            //double a = double.Parse(txtA.Text);
+            //double b = double.Parse(txtB.Text);
+            //double c = a + b;
+            //txtKetQua.Text = c.ToString();
+        }
     }
 }
